56fde07a42f4facd84144c3bf15c814fbcbf0e93
Luís Souza <luisduduovaes@gmail.com>
2025-03-07 16:10:29 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflito.ts
+++ conflito.ts
@@ -1,9 +1,5 @@
 console.log('Aluno A alterou o arquivo conflito.ts');
 console.log('Aluno B alterou o arquivo conflito.ts');
 
-<<<<<<< HEAD
-let calculo = (75 + 75) / 3;
-=======
 let concatenar: string = "Git + Hub"
 console.log('Concatenado ${concatenar}');
->>>>>>> e1737f98cf90a8bb3d6377e0fe594cd1f4a70e56
