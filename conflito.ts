e1737f98cf90a8bb3d6377e0fe594cd1f4a70e56
João Temponi <joao3gamerbr@gmail.com>
2025-03-07 15:51:54 -0300

    merge

--- conflito.ts
+++ conflito.ts
@@ -1,2 +1,5 @@
 console.log('Aluno A alterou o arquivo conflito.ts');
 console.log('Aluno B alterou o arquivo conflito.ts');
+
+let concatenar: string = "Git + Hub"
+console.log('Concatenado ${concatenar}');
